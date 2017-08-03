20ed3cc752ec975fedcbede7e9ec972661b11fc7
milak <milak@users.noreply.github.com>
2017-08-03 22:22:09 +0200

    Create osgi-rest-plugin.go

---
+++ osgi-rest-plugin.go
@@ -0,0 +1,20 @@
+package main
+import (
+	"log"
+	"github.com/milak/tools/osgi"
+)
+var SymbolicName 	string
+var Version 		string
+const SERVICE_NAME 	= "AMQP"
+var logger log.Logger
+func init(){
+	SymbolicName 	= "amqp-connector"
+	Version 		= "1.0"
+}
+func Start(context osgi.BundleContext){
+	logger = osgi.GetLoggerFromContext(context)
+	logger.Println("INFO Starting osgi-rest plugin")
+}
+func Stop(context osgi.BundleContext){
+  logger.Println("INFO Stopping osgi-rest plugin")
+}
