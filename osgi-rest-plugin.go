4465789aa0be81aa7362e0d2244294ee684ddaf2
milak <milak@users.noreply.github.com>
2017-08-03 22:47:10 +0200

    Update osgi-rest-plugin.go

--- osgi-rest-plugin.go
+++ osgi-rest-plugin.go
@@ -5,16 +5,15 @@ import (
 )
 var SymbolicName 	string
 var Version 		string
-const SERVICE_NAME 	= "AMQP"
 var logger *log.Logger
 func init(){
-	SymbolicName 	= "amqp-connector"
-	Version 		= "1.0"
+	SymbolicName 	= "osgi-rest"
+	Version 	= "1.0"
 }
 func Start(context osgi.BundleContext){
 	logger = osgi.GetLoggerFromContext(context)
 	logger.Println("INFO Starting osgi-rest plugin")
 }
 func Stop(context osgi.BundleContext){
-  logger.Println("INFO Stopping osgi-rest plugin")
+	logger.Println("INFO Stopping osgi-rest plugin")
 }
